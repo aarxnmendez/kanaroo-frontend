071594cdeabd029bc2941d8eda19538659364c09
Aaron Mendez <aarxnmendezz@gmail.com>
2025-05-04 09:22:06 +0200

    Feature: añadido componente VelocityScroll a la Landing para mejorar la separación entre secciones y el atractivo visual

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,6 +3,7 @@ import MainLayout from "@/layouts/MainLayout";
 import Hero from "@/components/landing/Hero";
 import Preview from "@/components/landing/Preview";
 import Benefits from "@/components/landing/Benefits";
+import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
 
 function Landing() {
   return (
@@ -11,6 +12,13 @@ function Landing() {
         <Hero />
         <Preview />
         <Benefits />
+        <VelocityScroll
+          defaultVelocity={3}
+          numRows={2}
+          className="text-xl md:text-3xl lg:text-4xl font-thin text-center text-muted-foreground w-9/10 lg:w-3/4 py-12 md:py-24 lg:py-32"
+        >
+          Organiza tu vida. Potencia tu trabajo. Logra tus metas.
+        </VelocityScroll>
       </MainLayout>
     </>
   );
