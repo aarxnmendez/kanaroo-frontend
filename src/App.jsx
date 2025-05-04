9038da02ab7ca65c5732bbf2eaef581f21d3a92d
Aaron Mendez <aarxnmendezz@gmail.com>
2025-05-04 09:45:53 +0200

    Feature: añadido componente NotFound e integrado en el enrutamiento para manejar errores 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
 import Landing from "@/pages/Landing";
+import NotFound from "@/pages/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
