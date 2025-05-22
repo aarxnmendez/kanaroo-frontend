61eef109d3a2ec107cb0af95020be50b09f76deb
Aaron Mendez <aarxnmendezz@gmail.com>
2025-05-23 00:57:04 +0200

    Update: comentados AppHeader y AppFooter en MainLayout para ajustes de diseño

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,15 @@
-import AppHeader from "@/components/app-header";
-import AppFooter from "@/components/app-footer";
+// import AppHeader from "@/components/app-header";
+// import AppFooter from "@/components/app-footer";
 
 function MainLayout({ children }) {
   return (
     <>
       <div className="flex min-h-screen flex-col items-center bg-background text-foreground">
-        <AppHeader />
+        {/* <AppHeader /> */}
         <main className="mx-auto flex w-full flex-1 flex-col items-center py-16 sm:py-20 md:py-24 px-0">
           {children}
         </main>
-        <AppFooter />
+        {/* <AppFooter /> */}
       </div>
     </>
   );
