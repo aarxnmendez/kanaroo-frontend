627ccba82bce6cee5a51ac503b562de4b2379354
Aaron Mendez <aarxnmendezz@gmail.com>
2025-05-25 23:23:01 +0200

    Refactor: se ha actualizado el componente MainLayout para incluir el nuevo Navbar y ScrollProgress, mejorando la estructura y la experiencia de navegación.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,17 +1,20 @@
-// import AppHeader from "@/components/app-header";
-// import AppFooter from "@/components/app-footer";
+import { Navbar } from "@/components/landing/Navbar";
+import { ScrollProgress } from "@/components/magicui/scroll-progress";
 
 function MainLayout({ children }) {
   return (
-    <>
-      <div className="flex min-h-screen flex-col items-center bg-background text-foreground">
-        {/* <AppHeader /> */}
-        <main className="mx-auto flex w-full flex-1 flex-col items-center py-16 sm:py-20 md:py-24 px-0">
+    <div className="min-h-dvh flex flex-col">
+      <div className="sticky top-0 z-50 w-full h-1/10">
+        <Navbar />
+        <ScrollProgress className="top-16 bg-foreground/50" />
+      </div>
+
+      <div className="flex flex-1 flex-col items-center justify-center bg-background text-foreground">
+        <main className="mx-auto flex w-full flex-1 flex-col justify-center items-center px-0">
           {children}
         </main>
-        {/* <AppFooter /> */}
       </div>
-    </>
+    </div>
   );
 }
 
