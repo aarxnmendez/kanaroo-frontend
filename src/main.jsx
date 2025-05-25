fbab53b4b8aa621685a6d88dfb9eb9e9960ab91e
Aaron Mendez <aarxnmendezz@gmail.com>
2025-05-25 23:15:59 +0200

    Refactor: reemplazado BrowserRouter con RouterProvider y configurado el enrutamiento utilizando createBrowserRouter en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routesConfig } from "./App.jsx";
 import "@/styles/index.css";
+
+const router = createBrowserRouter(routesConfig);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
