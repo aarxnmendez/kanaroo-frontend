2e9999e1a11f1c6371129353eff8be003e576f44
Aaron Mendez <aarxnmendezz@gmail.com>
2025-06-09 20:52:21 +0200

    Feature: agregar configuración de rutas para HomePage y LandingLayout en App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { useThemeStore } from "@/store/theme.store";
 
+import HomePage from '@/pages/Landing/HomePage.jsx';
+import LandingLayout from '@/layouts/LandingLayout.jsx';
+
 export function App() {
   const initializeTheme = useThemeStore((state) => state.initializeTheme);
 
@@ -16,5 +19,16 @@ export const routesConfig = [
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        element: <LandingLayout />,
+        children: [
+          {
+            index: true,
+            element: <HomePage />,
+          },
+        ],
+      },
+    ],
   },
 ];
